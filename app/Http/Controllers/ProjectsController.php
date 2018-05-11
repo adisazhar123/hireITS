f30e797837250fd6bad5d62ad8a5e7d80137362a
adis azhar <adisazhar123@gmail.com>
2018-05-11 17:05:42 +0700

    browse project using ajax pagination [DONE]

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -9,9 +9,12 @@ class ProjectsController extends Controller
 {
     public function index(Request $request){
       if (!isset($request->keywords) && empty($request->keywords) )
-        $jobs = Job::all();
+        $jobs = Job::paginate(1);
       else{
         $jobs = Job::where('name', 'like', '%'.$request->keywords.'%')->orWhere('description', 'like', '%'.$request->keywords.'%')->get();
+      }
+      if($request->ajax()){
+        return view('projects.project-list')->with('jobs', $jobs)->with('keyword', $request->keywords)->render();
       }
       return view('projects.browse-projects')->with('jobs', $jobs)->with('keyword', $request->keywords);
     }
--- resources/views/projects/browse-projects.blade.php
+++ resources/views/projects/browse-projects.blade.php
@@ -322,71 +322,14 @@
           </div>
         </div>
         <div class="col-md-9">
-          <div class="available-jobs mb-3" style="background-color: white;">
-            <div class="jobs-head mb-3" style="padding: 20px">
-              <select class="" name="">
-                <option value="">Newest First</option>
-                <option value="">Lowest Budget First</option>
-                <option value="">Highest Budget First</option>
-                <option value="">Lowest bid/entries</option>
-                <option value="">Highest bid/entries</option>
-              </select>
-            </div>
+          <div class="jobss">
+            @include('projects.project-list')
 
-            @foreach ($jobs as $job)
-              <div class="jobs-body">
-                <a class="body-link" href="{{route('view.project',['slug' => $job->slug])}}" style="display: block">
-                  <div class="row">
-                    <div class="col-md-10">
-                      <div class="row">
-                        <h4>{{$job->name}}</h4>
-                        <h6>
-                          @php
-                            $now = date_create(date("d-m-Y"));
-                            $end = date_create(date_format(date_create($job->deadline), "d-m-Y"));
-                            $diff=date_diff($now, $end);
-                            echo $diff->format('%d day/s left');
-                          @endphp
-                        </h6>
-                      </div>
-                      <div class="row">
-                        <div class="job-desc">
-                          <p>
-                            {{substr(strip_tags($job->description), 0, 300)}}
-                          </p>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="job-tags">
-                          <a href="#">php </a>
-                          <a href="#">photoshop </a>
-                          <a href="#">laravel </a>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-md-2">
-                      <div class="row">
-                        <div class="job-price">
-                          <h4>
-                            ${{$job->price_min}}
-                            -
-                            ${{$job->price_max}}
-                          </h4>
-                          <small>(avg bid)</small>
-                          <p>{{$job->no_of_bids}} bids</p>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <a href="#" class="btn bid-btn" type="button" name="button">bid now</a>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            @endforeach
-
-            </div>
           </div>
+
+        </div>
+
+
         </div>
       </div>
     </div>
@@ -418,6 +361,28 @@
       $(".filter-content").addClass("show");
     }
   });
+
+  $('body').on('click', '.pagination a', function(e) {
+    e.preventDefault();
+    $('#load a').css('color', '#dfecf6');
+     $('#load').append('<img style="position: absolute; left: 30%; top: 0; z-index: 100000;" src="{{asset('loading.gif')}}" alt="haha" />');
+
+    var url = $(this).attr('href');
+    getJobs(url);
+    window.history.pushState("", "", url);
+});
+
+function getJobs(url){
+  $.ajax({
+            url : url
+        }).done(function (data) {
+            $('.jobss').html(data);
+        }).fail(function () {
+            alert('jobs could not be loaded.');
+        });
+    }
+
+
 
 
   </script>
